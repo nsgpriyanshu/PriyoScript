8fc81c53e7236870777883609b348d90d1c10b14
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2026-03-01 12:45:37 +0530

    fix: navbar add docs link

--- web/app/(home)/layout.tsx
+++ web/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import { HomeLayout } from 'fumadocs-ui/layouts/home'
 import { Geist_Mono } from 'next/font/google'
 import { baseOptions } from '@/lib/layout.shared'
+import { BookMarkedIcon, Mail } from 'lucide-react'
 
 const geistMono = Geist_Mono({
   subsets: ['latin'],
@@ -8,8 +9,15 @@ const geistMono = Geist_Mono({
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <HomeLayout {...baseOptions()}>
+    <HomeLayout {...baseOptions()}
+    links={[{
+        icon: <BookMarkedIcon />,
+        text: 'Docs',
+        url: '/docs/stable',
+        secondary: false,
+      },]}>
       <div className={geistMono.className}>{children}</div>
     </HomeLayout>
   )
 }
+  
